ae0d8d094434dfe02be92294035e43d51d3da765
Ahmed Essam El-Din Rizk <105478629+ahmedessamrizk@users.noreply.github.com>
2024-08-10 23:44:55 +0300

    Add files via upload

---
+++ backend/src/modules/categories/categories.service.js
@@ -0,0 +1,151 @@
+import slugify from 'slugify';
+import { categoryModel } from '../../../DB/models/category.model.js';
+import { paginate } from '../../utils/pagination.js';
+import * as productsService from '../products/products.service.js';
+
+const privateData = '-__v -type';
+
+export const getCategory = async query => {
+  return await categoryModel.findOne(query);
+};
+
+export const create = async (category, currUser) => {
+  //slug the name
+  category.slug = slugify(category.name.toLowerCase());
+
+  //check if category already exists
+  if (await getCategory({ slug: category.slug })) {
+    throw new Error('category already exists', { cause: 409 });
+  }
+
+  //handle subcategory type
+  if (category.parentCategory) {
+    //check if parent category exists
+    const parentCategory = await getCategory({
+      _id: category.parentCategory
+    });
+    if (!parentCategory) {
+      throw new Error('parent category not found', { cause: 404 });
+    }
+
+    //subcategory type is more than direct parent type by 1
+    category.type = parentCategory.type + 1;
+  }
+
+  category.createdBy = currUser._id;
+  return await categoryModel.create(category);
+};
+
+export const getCategories = async query => {
+  //in case of getting all categories for admin panel, don't format the categories.
+  let send_query = query.format?.toLowerCase() === 'true' ? { type: 0 } : {};
+  const { limit, skip } = paginate(query.page, query.size);
+
+  const [categories, totalCategories] = await Promise.all([
+    categoryModel
+      .find(send_query)
+      .limit(limit)
+      .skip(skip)
+      .select(privateData)
+      .populate({ path: 'createdBy', select: 'userName email' }),
+    categoryModel.countDocuments(send_query)
+  ]);
+
+  // Calculate the number of pages available
+  const totalPages = Math.ceil(totalCategories / limit);
+  let result = categories;
+
+  //reformat categories if format query is true
+  if (query.format?.toLowerCase() === 'true') {
+    result = await reformatCategories(categories);
+  }
+
+  return {
+    total: totalCategories,
+    totalPages,
+    categories: result
+  };
+};
+
+export const updateCategory = async (id, category) => {
+  category.slug = slugify(category.name.toLowerCase());
+
+  const updatedCategory = await categoryModel.findByIdAndUpdate(id, category, {
+    new: true
+  });
+
+  if (!updatedCategory) {
+    throw new Error('category not found', { cause: 404 });
+  }
+  return updatedCategory;
+};
+
+export const deleteCategory = async id => {
+  const subCategories = await getCategory({ parentCategory: id });
+
+  //check if category doesn't have subcategories
+  if (subCategories) {
+    throw new Error('Category has subcategories and cannot be deleted.', {
+      cause: 400
+    });
+  }
+
+  //check if category is used in at least one product
+  const product = await productsService.getProduct({ category: id });
+  if (product) {
+    throw new Error('Category has products and cannot be deleted.', {
+      cause: 400
+    });
+  }
+
+  const deletedCategory = await categoryModel.findByIdAndDelete(id);
+
+  if (!deletedCategory) {
+    throw new Error('category not found', { cause: 404 });
+  }
+};
+
+export const checkValidCategory = async query => {
+  let category;
+  if (query) {
+    category = await getCategory(query);
+    if (!category) {
+      return { status: false, message: 'category not found', cause: 404 };
+    }
+  }
+  return { status: true, _id: category._id };
+};
+
+const reformatCategories = async paginatedCategories => {
+  //get all subcategories
+  const categories = await categoryModel.find({
+    type: { $in: [1, 2] }
+  });
+
+  //add paginated root categories to the subcategories
+  categories.push(...paginatedCategories);
+
+  // Create a map of categories by their ID where key is the category ID and value is the category object
+  const categoryMap = {};
+  categories.forEach(category => {
+    categoryMap[category._id] = { ...category.toObject(), subCategories: [] };
+  });
+
+  // Intialize array for reformated categories
+  const result = [];
+
+  // Iterate over categories and add subcategories to their parent category
+  categories.forEach(category => {
+    if (category.parentCategory) {
+      const parent = categoryMap[category.parentCategory];
+      if (parent) {
+        delete categoryMap[category._id].parentCategory;
+        parent.subCategories.push(categoryMap[category._id]);
+      }
+    } else {
+      result.push(categoryMap[category._id]);
+    }
+  });
+
+  return result;
+};
---
+++ backend/src/modules/users/users.controller.js
@@ -0,0 +1,33 @@
+import { roles } from '../../middleware/auth.js';
+import { createResponse } from '../../utils/createResponse.js';
+import { asyncHandler } from '../../utils/errorHandlingService.js';
+import * as usersService from './users.service.js';
+
+export const getProfile = asyncHandler(async (req, res) => {
+  return res.json(createResponse(200, { user: req.user }));
+});
+
+export const logout = asyncHandler(async (req, res) => {
+  res.clearCookie('token',{
+    httpOnly: true,
+    sameSite: 'none',
+    secure: true
+  });
+  return res.json(createResponse(200));
+});
+
+export const getUsers = asyncHandler(async (req, res) => {
+  const { page, size } = req.query;
+  const result = await usersService.getUsers({ page, size });
+  return res.json(createResponse(200, result));
+});
+
+export const removeUser = asyncHandler(async (req, res) => {
+  await usersService.removeUser(req.params.id, roles.User, true);
+  return res.status(200).json(createResponse(200));
+});
+
+export const unRemoveUser = asyncHandler(async (req, res) => {
+  await usersService.removeUser(req.params.id, roles.User, false);
+  return res.status(200).json(createResponse(200));
+});
