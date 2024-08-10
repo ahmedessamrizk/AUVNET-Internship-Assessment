aab66cb442229003831e0027035675ac1fe7fb33
Ahmed Essam El-Din Rizk <105478629+ahmedessamrizk@users.noreply.github.com>
2024-08-10 17:16:05 +0300

    Add files via upload

---
+++ backend/src/modules/auth/auth.controller.js
@@ -0,0 +1,35 @@
+import { createResponse } from '../../utils/createResponse.js';
+import * as authService from './auth.service.js';
+import { asyncHandler } from '../../utils/errorHandlingService.js';
+
+export const signUp = asyncHandler(async (req, res, next) => {
+  const user = await authService.signUp(req);
+  if (user) {
+    return res
+      .status(201)
+      .json(createResponse(201, 'please confirm your email before login'));
+  }
+  return next(Error('Failed to signUp', { cause: 400 }));
+});
+
+export const confirmEmail = asyncHandler(async (req, res, next) => {
+  const { token } = req.params;
+
+  await authService.confirmEmail(token);
+
+  return process.env.MODE == 'DEV'
+    ? res.status(200).json(createResponse(200))
+    : res.redirect(process.env.frontendBaseURL);
+});
+
+export const signIn = asyncHandler(async (req, res, next) => {
+  const { userName, password } = req.body;
+  const token = await authService.signIn(userName, password);
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    sameSite: 'none',
+    secure: true
+  });
+  return res.status(200).json(createResponse(200));
+});
