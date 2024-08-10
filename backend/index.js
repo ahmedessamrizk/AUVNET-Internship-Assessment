be8f2fa6c767b74fd0f9dc58037d73c2239beb97
Ahmed Essam El-Din Rizk <105478629+ahmedessamrizk@users.noreply.github.com>
2024-08-10 23:44:16 +0300

    Delete backend directory

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { appRouter } from './src/app.js';
-
-const app = express();
-appRouter(app);
-
-app.listen(process.env.PORT, () =>
-  console.log(`Server is running on port ${process.env.PORT}!`)
-);
